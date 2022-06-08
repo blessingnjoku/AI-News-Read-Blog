b7e64df2cc373d0e46623a7b3b9dac782cb6e1c3
Blessing Njoku <blessingnjoku505@gmail.com>
2022-06-08 22:48:22 +0100

    activating Alan icon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import { useEffect } from "react";
 
-function App() {
+const alankey =
+  "65b8f7219be52ad83e66196cd56bd95d2e956eca572e1d8b807a3e2338fdd0dc/stage";
+const App = () => {
+  useEffect(() => {
+    alanBtn({
+      key: alankey,
+      onCommand: ({ command, articles }) => {
+        if (command === "newHeadlines") {
+          console.log(articles);
+        }
+      },
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>this is working</h1>
     </div>
   );
-}
+};
 
 export default App;
